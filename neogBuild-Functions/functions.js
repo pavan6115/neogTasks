06fcf60e04cc42162912f9ebc344055ff4be7f14
kpavann <kulkarnipavan6115@gmail.com>
2022-01-12 17:55:35 +0530

    feat : easy section done

---
+++ neogBuild-Functions/functions.js
@@ -0,0 +1,73 @@
+// Author : Pavan Kulkarni
+
+// Easy Section - 
+
+```
+1 - 
+Given a and b, your function should return the value of ab
+Example:
+Input: power(2,3) ––> Output: 8
+```
+const power = (a,b) => a**b;
+power(2,3) // 8
+
+
+```
+2 -
+Given length of a regular hexagon, your function should return area of the hexagon.
+Example:
+Input: areaOfHexagon(10) ––> Output: 259.80
+```
+const areaofHeax = c => 2.598 * c;
+areaofHeax(10) // 25.98
+
+
+```
+3 -
+Given a sentence, your functions should return the number of words in the sentence.
+Example:
+Input: noOfWords(We are neoGrammers) ––> Output: 3
+```
+const noOfWords = char => char.split(' ').length
+noOfWords('We are neoGrammers') // 3
+
+
+```
+4 -
+Given n numbers, your function should return the minimum of them all. The number of parameters won't be accepted from user.
+Example:
+Input: findMin(3,5) ––> Output: 3
+Input: findMin(3,5,9,1) ––> Output: 1
+(Hint: Lookup rest parameters in JavaScript)
+```
+function findMin(...args){
+    return Math.min(...args)
+}
+findMin(3,5,6,1)  //1
+
+
+```
+5 - 
+Given n numbers, your function should return the maximum of them all. The number of parameters won't be accepted from user.
+Example:
+Input: findMax(3,5) ––> Output: 5
+Input: findMax(3,5,9,1) ––> Output: 9
+(Hint: Lookup rest parameters in JavaScript)
+```
+function findMax(...args){
+    return Math.max(...args)
+}
+findMax(3,4,6,1)  //6
+
+
+```
+6 -
+Given three angles of a triange, your function should return if it is a scalene, isosceles, equilateral triangle or not a triangle at all. Example:
+Input: typeOfTriangle(30, 60, 90) ––> Output: Scalene Triangle
+```
+const typeOfTriangle = (a,b,c) => {
+    if (a === b && b === c) return `Equilateral Triangle`
+    else if (a === b || b === c || a === c) return `Isosceles Triangle`
+    else return `Scalene Triangle`
+}
+typeOfTriangle(30,90,30)  //Isoceles Triangle
