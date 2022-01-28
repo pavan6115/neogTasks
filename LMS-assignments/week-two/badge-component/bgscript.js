4d3f0369808d710bb08adf5230de0878fabe822b
kpavann <kulkarnipavan6115@gmail.com>
2022-01-28 11:07:23 +0530

    refactor : added functionality

--- LMS-assignments/week-two/badge-component/bgscript.js
+++ LMS-assignments/week-two/badge-component/bgscript.js
@@ -1,11 +1,11 @@
 console.log('connected!')
 
-const showToggleBtn = document.querySelector('#btn-toggle-bg')
-const showToggleNum = document.querySelector('#circle-show')
+const showToggleBtn = document.querySelector('.btn-toggle-bg')
+const showToggleNum = document.querySelector('.circle-show')
 
 showToggleBtn.addEventListener('click', () => {
-    if (showToggleBtn.style.display === "none"){
-        showToggleBtn.style.display = "block";
+    if (showToggleNum.style.display === "none") {
+        showToggleNum.style.display = "block"
     }
-    else showToggleBtn.style.display = "none";
-});
+    else showToggleNum.style.display = "none"
+})
